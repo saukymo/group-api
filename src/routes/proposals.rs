71674fe57da432c9f6815921c208d0091f925f3d
Shaobo <shaobo_liu2@apple.com>
2022-03-02 14:48:26 +0800

    Add offline features for sqlx.

--- src/routes/proposals.rs
+++ src/routes/proposals.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::future::Future;
 use tide::Response;
 
 use crate::models::models::{NewProposal, Proposal, Appointment};
@@ -29,31 +30,7 @@ pub async fn create_proposal(mut request: Request<State>) -> Result {
 }
 
 pub async fn get_proposal(request: Request<State>) -> Result {
-    let proposal_id = request.param("proposal_id")?.parse::<i32>()?;
-
-    let db_pool = request.state().pool.clone();
-
-    let result = Proposal::get_proposal_by_proposal_id(proposal_id, &db_pool).await?;
-
-    let res = match result {
-        Some(proposal) => {
-            let mut response = Response::new(StatusCode::Ok);
-            response.set_body(Body::from_json(&json!({
-                "proposal": proposal
-            }))?);
-            response
-        },
-        None => {
-            let mut response = Response::new(StatusCode::NotFound);
-            response.set_body(Body::from_json(&json!({
-                "status": "error",
-                "message": "Proposal ID not found."
-            }))?);
-            response
-        }
-    };
-
-    Ok(res)
+    check_proposal_id_before(return_proposal, request).await
 }
 
 pub async fn update_proposal(request: Request<State>) -> Result {
@@ -69,15 +46,51 @@ pub async fn delete_proposal(request: Request<State>) -> Result {
 }
 
 pub async fn get_appointments(request: Request<State>) -> Result {
-    let proposal_id = request.param("proposal_id")?.parse::<i32>()?;
+    check_proposal_id_before(get_appointments_by_proposal_id, request).await
+}
 
+async fn get_appointments_by_proposal_id(proposal: Proposal, request: Request<State>) -> Result {
     let db_pool = request.state().pool.clone();
 
-    let appointments = Appointment::get_appointments_by_proposal_id(proposal_id, &db_pool).await?;
+    let appointments = Appointment::get_appointments_by_proposal_id(proposal.proposal_id, &db_pool).await?;
 
-    let mut res = Response::new(StatusCode::Ok);
-    res.set_body(Body::from_json(&json!({
+    let mut response = Response::new(StatusCode::Ok);
+    response.set_body(Body::from_json(&json!({
         "appointments": appointments
     }))?);
-    Ok(res)
+
+    Ok(response)
+}
+
+async fn check_proposal_id_before<F, Fut>(f: F, request: Request<State>) -> Result 
+    where 
+        F: FnOnce(Proposal, Request<State>) -> Fut,
+        Fut: Future<Output = Result>
+{
+    let proposal_id = request.param("proposal_id")?.parse::<i32>()?; 
+    let db_pool = request.state().pool.clone();
+
+    if let Some(proposal) = Proposal::get_proposal_by_proposal_id(proposal_id, &db_pool).await?{
+        return f(proposal, request).await;
+    } else {
+        return proposal_not_found(proposal_id).await;
+    }
+}
+
+
+async fn proposal_not_found(proposal_id: i32) -> Result {
+    let mut response = Response::new(StatusCode::NotFound);
+    response.set_body(Body::from_json(&json!({
+        "status": "error",
+        "message": format!("Proposal ID {proposal_id} not found.")
+    }))?);
+    Ok(response)
+}
+
+async fn return_proposal(proposal: Proposal, _request: Request<State>) -> Result {
+    let mut response = Response::new(StatusCode::Ok);
+    response.set_body(Body::from_json(&json!({
+        "proposal": proposal
+    }))?);
+    Ok(response)
 }
